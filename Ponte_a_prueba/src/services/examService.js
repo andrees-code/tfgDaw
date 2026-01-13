98fd8dc89210248c607697b5f8191c10e0b1392f
andresito2004 <dronmelinton@gmail.com>
2026-01-13 22:01:43 +0100

    Va cogiendo ritmo, modificando /api/api

--- Ponte_a_prueba/src/services/examService.js
+++ Ponte_a_prueba/src/services/examService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { userStore } from "@/stores/userStores"
 
 const api = axios.create({
-  baseURL: 'https://bakend-murex.vercel.app/api',
+  baseURL: 'https://bakend-murex.vercel.app/api/api',
   headers: { 'Content-Type': 'application/json' },
 })
 

9dd9fa467665b84b5e16a9d21d364d13b7c1fa68
andresito2004 <dronmelinton@gmail.com>
2026-01-16 19:36:26 +0100

    Añadidas analytics de vercel

--- Ponte_a_prueba/src/main.js
+++ Ponte_a_prueba/src/main.js
@@ -3,12 +3,16 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import { userStore } from '@/stores/userStores'
+import { inject } from '@vercel/analytics'
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(createPinia())
+
+// 🔹 Activar Vercel Analytics
+inject()
 
 // 🔹 Cargar sesión antes de montar la app
 userStore.loadSession()
